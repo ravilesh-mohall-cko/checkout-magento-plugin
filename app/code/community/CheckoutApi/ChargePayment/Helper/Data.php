6c33399048b8bf3debf7a552c18101aebb4f8b0a
dhiraj-gangoosirdar-cko <dhiraj.gangoosirdar@checkout.com>
2015-06-05 08:21:06 +0400

    fixing https

--- app/code/community/CheckoutApi/ChargePayment/Helper/Data.php
+++ app/code/community/CheckoutApi/ChargePayment/Helper/Data.php
@@ -16,7 +16,7 @@ class CheckoutApi_ChargePayment_Helper_Data  extends Mage_Core_Helper_Abstract
        $mode =  $this->getConfigData('mode','creditcard');
         $js = '<script src="https://www.checkout.com/cdn/js/checkout.js" async ></script>';
        if($mode == 'sandbox') {
-           $js ='<script src="http://sandbox.checkout.com/js/v1/checkout.js" async ></script>';
+           $js ='<script src="https://sandbox.checkout.com/js/v1/checkout.js" async ></script>';
        }
         return $js;
     }
