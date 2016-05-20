e4f789e224e023b15ad2f4d73a0c25b97a37ce6d
ravish-ramrakha-cko <ravish.ramrakha@checkout.com>
2016-05-20 13:22:04 +0400

    Update Constant.php

--- lib/CheckoutApi/Client/Constant.php
+++ lib/CheckoutApi/Client/Constant.php
@@ -22,7 +22,7 @@ final class CheckoutApi_Client_Constant
 	const TOKEN_SESSION_TYPE = 'sessionToken';
 	const AUTOCAPUTURE_CAPTURE = 'y';
 	const AUTOCAPUTURE_AUTH = 'n';
-	const VERSION = '1.2.3';
+	const VERSION = 'v2';
 
 
 }
